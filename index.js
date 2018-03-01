60e7ef9aa5d3ea0ae079cd65ab43668f9ba598b0
Benjamin Hoffmeyer <hoffmeyer25@gmail.com>
2018-03-01 15:08:24 -0500

    Who even needs translations anymore
    In a seperate commit from reset of the changes to allow for reverting
    this commit more easily

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 require('dotenv').config()
 
 global.logger = require('./src/internal/logger')
-global.i18n = require('./src/internal/i18n')
 global.logger.log('Beginning startup sequence...')
 
 require('./src/internal/check-env')
