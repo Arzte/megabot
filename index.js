ded435364d10ce7535eb998d33dc9e10bb9ca538
Benjamin Hoffmeyer <hoffmeyer25@gmail.com>
2018-03-01 10:46:42 -0500

    add megabot parts (eww)

--- index.js
+++ index.js
@@ -1,45 +1,14 @@
-require('dotenv').config()
-
-global.logger = require('./src/internal/logger')
-global.i18n = require('./src/internal/i18n')
-global.logger.log('Beginning startup sequence...')
-
-require('./src/internal/check-env')
-require('./src/internal/version-check')
-
-const Eris = require('eris')
-const Events = require('./src/internal/directory-loader')('./src/events')
-const bot = new Eris(process.env['BOT_TOKEN'], {
-  restMode: true
+const Discordie = require('discordie')
+const Config = require('./config.js')
+const Events = require('./src/events/index')
+const Client = new Discordie({
+  autoReconnect: true
 })
 
-bot._ogEmit = bot.emit
-bot.emit = function emit () {
-  this._anyListeners.forEach(listener => listener.apply(this, [arguments]))
-  return this._ogEmit.apply(this, arguments) // eslint-disable-line
-}
-bot.onAny = function onAny (func) {
-  if (!this._anyListeners) this._anyListeners = []
-  this._anyListeners.push(func)
-}
-
-bot.onAny((ctx) => {
-  if (Events[ctx[0]]) {
-    // global.logger.debug(`Found listener for event '${ctx[0]}'`)
-    Events[ctx[0]](Array.from(ctx).slice(1))
-  } // else Logger.debug(`No listener for '${ctx[0]}' found`)
+Client.Dispatcher.onAny((type, ctx) => {
+  if (Events[type]) Events[type](ctx)
 })
 
-process.on('unhandledRejection', (err) => {
-  global.logger.error(err)
-})
-
-process.on('uncaughtException', (err) => {
-  // probably not the most stylish way to handle this, but it works
-  global.logger.error(err, true) // we're exiting here, uncaughts are scary
-})
-
-bot.connect().then(() => {
-  global.bot = bot
-  require('./src/internal/bezerk')
+Client.connect({
+  token: Config.Discord.token
 })
