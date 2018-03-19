fd6e9bf49f7d84157430e4b3e45e837f3ae84e13
Benjamin Hoffmeyer <doctors@doc.tors.life>
2018-03-19 15:00:38 -0400

    Do DB operations when action is taken with item, rather than every time

--- src/commands/reactions/approve.js
+++ src/commands/reactions/approve.js
@@ -7,22 +7,19 @@ module.exports = async (ctx) => {
     const emoji = msg.channel.guild.emojis
     const approve = emoji.find(emoji => emoji.name === 'approve')
     const f1 = emoji.find(emoji => emoji.name === 'f1')
-    const card = r.table('queue').get(msg.id)
-    const doc = await card.run()
-    await doc.update({
-      approvers: r.row('approvers').default([]).append(msg.author.id),
-      approved: r.row('approved').add(1).default(0)
-    }).run()
-    doc.approved++
-    if (doc.approved === Constants.Guild.reportThres) {
-      var reportsArr = await msg.fetchReactions(`${approve.name}:${approve.id}`)
-      for (let reaction in reportsArr) {
-        await msg.removeReaction(`${approve.name}:${approve.id}`, reaction[0])
-      }
-      await msg.addReaction(`${f1.name}:${f1.id}`)
+
+    if (await msg.getReaction(`${approve.name}:${approve.id}`).length() === Constants.Guild.reportThres) {
+      const card = await r.table('queue').get(msg.id).run()
+      const userReactArray = await msg.getReactions(`${approve.name}:${approve.id}`, 100, '', bot.user)
+      const userIdArray = userReactArray.map(user => user.id)
+
+      await card.update({
+        approvers: r.row('approvers').default([]).append(userIdArray),
+        approved: r.row('approved').add(userIdArray.length).default(0)
+      }).run()
       // TODO: Send a card to the queue, can't be bothered to setup that boilerplate right now.
     }
-    await card.run()
+
     const message = await msg.addReaction(`${f1.name}:${f1.id}`)
     setTimeout(() => message.removeReaction(`${f1.name}:${f1.id}`), Constants.Timeouts.removeReaction)
   } catch (error) {
