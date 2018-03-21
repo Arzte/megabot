16a7b477afb48016e85eed50b2888361e9686fbd
Benjamin Hoffmeyer <doctors@doc.tors.life>
2018-03-21 16:13:49 -0400

    Fix typo in comment

--- src/events/messageReactionAdd.js
+++ src/events/messageReactionAdd.js
@@ -2,7 +2,7 @@ const Reactions = require('../internal/directory-loader')('./src/commands/reacti
 
 module.exports = async function (ctx) {
   try {
-    // We don't get the message author outside of an id, so we need to get that object
+    // We don't get the message author object, just an id, so we need to get that object
     if (await bot.getRESTUser(ctx[2]).bot) return
     if (Reactions[ctx[1].name]) {
       Reactions[ctx[1].name](ctx)
