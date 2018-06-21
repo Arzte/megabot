9e190b100e2f2dd66c97b03762dfa7cd303305de
Anastasia Hoffmeyer <hoffmeyer25@gmail.com>
2018-06-21 11:35:02 -0400

    Push eslint fixes, ES6 stuff mostly.

--- src/commands/reactions/approve.js
+++ src/commands/reactions/approve.js
@@ -1,46 +1,46 @@
-const Constants = require('../../internal/constants')
+import { Guild, UV, Emoji, Timeouts } from '../../internal/constants'
+import { table, row } from '../../models/rethinkdb'
 const Helpers = {
   login: require('../../engines/login')
 }
-const r = require('../../models/rethinkdb')
 
-module.exports = async (ctx) => {
+export default async (ctx) => {
   try {
     // We need to fetch the reaction, in case the message isn't cached by the bot.
     const msg = await bot.getMessage(ctx[0].channel.id, ctx[0].id)
-    const card = await r.table('queue').get(msg.id).run()
+    const card = await table('queue').get(msg.id).run()
     const client = await Helpers.login.owner
     const result = await client.get(`admin/suggestions/${card.UvId}`)
     const suggestion = result.suggestions[0]
-    const feedMessage = await bot.createMessage(Constants.Guild.pubFeed, {
+    const feedMessage = await bot.createMessage(Guild.pubFeed, {
       title: suggestion.title,
       description: suggestion.body,
       url: suggestion.portal_url,
       timestamp: suggestion.created_at
     })
     const react = async () => {
-      await feedMessage.addReaction(Constants.UV.Emoji.upvote)
-      await feedMessage.addReaction(Constants.UV.Emoji.report)
+      await feedMessage.addReaction(UV.Emoji.upvote)
+      await feedMessage.addReaction(UV.Emoji.report)
     }
 
-    if (msg.channel.id === Constants.Guild.feed) {
-      if (await msg.getReaction(Constants.UV.Emoji.approve).length === Constants.Guild.reportThres) {
-        const userReactArray = await msg.getReaction(Constants.Emoji.approve, 100, '', bot.user)
+    if (msg.channel.id === Guild.feed) {
+      if (await msg.getReaction(UV.Emoji.approve).length === Guild.reportThres) {
+        const userReactArray = await msg.getReaction(Emoji.approve, 100, '', bot.user)
         const userIdArray = userReactArray.map(user => user.id)
         await react
         await card.update({
-          id: r.row('id').update(feedMessage.id),
-          approvers: r.row('approvers').default([]).append(userIdArray),
-          approved: r.row('approved').uppdate(userIdArray.length).default(0)
+          id: row('id').update(feedMessage.id),
+          approvers: row('approvers').default([]).append(userIdArray),
+          approved: row('approved').uppdate(userIdArray.length).default(0)
         }).run()
         msg.delete('approved by custodians.')
       }
 
-      const message = await msg.addReaction(Constants.UV.Emoji.f1)
-      setTimeout(() => message.removeReaction(Constants.UV.Emoji.f1), Constants.Timeouts.removeReaction)
-    } else if (msg.channel.id === Constants.Guild.adminFeed) {
-      const message = await msg.addReaction(Constants.UV.Emoji.f1)
-      setTimeout(() => message.delete('Approved by Employee'), Constants.Timeouts.removeReaction)
+      const message = await msg.addReaction(UV.Emoji.f1)
+      setTimeout(() => message.removeReaction(UV.Emoji.f1), Timeouts.removeReaction)
+    } else if (msg.channel.id === Guild.adminFeed) {
+      const message = await msg.addReaction(UV.Emoji.f1)
+      setTimeout(() => message.delete('Approved by Employee'), Timeouts.removeReaction)
       await react
     }
   } catch (error) {
--- src/commands/reactions/deny.js
+++ src/commands/reactions/deny.js
@@ -1,24 +1,24 @@
-const Constants = require('../../internal/constants')
+import { Guild, UV, Emoji, Timeouts } from '../../internal/constants'
+import { table, row } from '../../models/rethinkdb'
 const Helpers = {
   login: require('../../engines/login')
 }
-const r = require('../../models/rethinkdb')
 
-module.exports = async (ctx) => {
+export default async (ctx) => {
   try {
     // We need to fetch the reaction, in case the message isn't cached by the bot.
     const msg = await bot.getMessage(ctx[0].channel.id, ctx[0].id)
-    const card = await r.table('queue').get(msg.id).run()
+    const card = await table('queue').get(msg.id).run()
     const client = await Helpers.login.owner
 
-    if (msg.channel.id === Constants.Guild.feed) {
-      if (await msg.getReaction(Constants.UV.Emoji.approve).length === Constants.Guild.reportThres) {
-        const userReactArray = await msg.getReaction(Constants.Emoji.deny, 100, '', bot.user)
+    if (msg.channel.id === Guild.feed) {
+      if (await msg.getReaction(UV.Emoji.approve).length === Guild.reportThres) {
+        const userReactArray = await msg.getReaction(Emoji.deny, 100, '', bot.user)
         const userNameArray = userReactArray.map(user => user.name)
         const userIdArray = userReactArray.map(user => user.id)
         const result = await client.get(`admin/suggestions/${card.UvId}`)
         const suggestion = result.suggestions[0]
-        const feedMessage = await bot.createMessage(Constants.Guild.adminFeed, {
+        const feedMessage = await bot.createMessage(Guild.adminFeed, {
           content: '**The following card was denied in queue, confirming this report will _remove_ this suggestion:**',
           embed: {
             title: suggestion.title,
@@ -31,21 +31,21 @@ module.exports = async (ctx) => {
             }]
           }
         })
-        await feedMessage.addReaction(Constants.UV.Emoji.approve)
-        await feedMessage.addReaction(Constants.UV.Emoji.deny)
+        await feedMessage.addReaction(UV.Emoji.approve)
+        await feedMessage.addReaction(UV.Emoji.deny)
         await card.update({
-          id: r.row('id').update(feedMessage.id),
-          approvers: r.row('deniers').default([]).append(userIdArray),
-          approved: r.row('denied').uppdate(userIdArray.length).default(0)
+          id: row('id').update(feedMessage.id),
+          approvers: row('deniers').default([]).append(userIdArray),
+          approved: row('denied').uppdate(userIdArray.length).default(0)
         }).run()
         await msg.delete('denied by custodians.')
       }
 
-      const reacMessage = await msg.addReaction(Constants.UV.Emoji.f1)
-      setTimeout(() => reacMessage.removeReaction(Constants.UV.Emoji.f1), Constants.Timeouts.removeReaction)
-    } else if (msg.channel.id === Constants.Guild.adminFeed) {
-      const message = await msg.addReaction(Constants.UV.Emoji.f1)
-      setTimeout(() => message.delete('Denied by Employee'), Constants.Timeouts.removeReaction)
+      const reacMessage = await msg.addReaction(UV.Emoji.f1)
+      setTimeout(() => reacMessage.removeReaction(UV.Emoji.f1), Timeouts.removeReaction)
+    } else if (msg.channel.id === Guild.adminFeed) {
+      const message = await msg.addReaction(UV.Emoji.f1)
+      setTimeout(() => message.delete('Denied by Employee'), Timeouts.removeReaction)
       await client.delete(`admin/suggestions/${card.UvId}`)
     }
   } catch (error) {
